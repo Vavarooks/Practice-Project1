ed0137a232ef7bc2d6ede9bb35426877761c2dc2
Vavarooks <vavarooks123@gmail.com>
2020-05-25 12:05:03 -0700

    tenth push

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,52 +1,11 @@
 import React from "react";
-// import Form from "./Form";
-// import About from "./About";
-// import { Link } from "react-router-dom";
 
-
-{/* <Route path="./Form" component={Form} />
-
-class App extends React.Component {
-    render() {
-        return (
-          <div>
-            <Link to="./Form" className="btn btn-primary">hello</Link>
-         </div>
-       ); 
-    }
-}
-
-class Second extends React.Component {
-    render() {
-        return <label>second component</label>
-    }
-}
-
-React.render( <App /> , document.getElementById('app')) */}
-
-
-
-// function Commissions() {
-//     return (
-//         <Form />
-//     )
-// }
-
-// function Info() {
-//     return (
-//         <About />
-//     )
-// }
 
 function Home() {
     return (
         <div>
-            <nav className="navbar navbar-light bg-light">
-                <form className="form-inline">
-                    <a href="./About.js" role="button" className="btn btn-outline-info" data-toggle="button" aria-pressed="false">About</a>
-                    <button href="/" type="button" className="btn btn-outline-primary" data-toggle="button" aria-pressed="false">Home</button>
-                    <button href="./FormUse.html" type="button" className="btn btn-outline-secondary" data-toggle="button" aria-pressed="false">Commissions</button>
-                </form>
+            <nav class="navbar fixed-top navbar-light bg-light">
+                <a class="navbar-brand" href="#">Welcome</a>
             </nav>
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
@@ -76,20 +35,62 @@ function Home() {
                         </div>
                     </div>
                 </div>
-            </div>
-            <footer className="page-footer font-small cyan darken-3">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 py-5">
-                            <div className="mb-5 flex-center">
+                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li className="nav-item">
+                        <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Commissions</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                    </li>
+                </ul>
+                <div className="tab-content" id="pills-tabContent">
+                    <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"><div className="jumbotron jumbotron-fluid">
+                        <div className="container">
+                            <h1 className="display-4">Commission Page</h1>
+                            <form action="mailto:vavarooks@aol.com" method="post" enctype="text/plain">
+                                <div className="form-group">
+                                    <label for="exampleFormControlInput1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                    </div></div>
+                    <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"><div className="jumbotron jumbotron-fluid">
+                        <div className="container">
+                            <h1 className="display-4">About Page</h1>
+                            <div className="card mb-3">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Artist</h5>
+                                    <p className="card-text">Makes the art that is commisioned!</p>
+                                </div>
+                            </div>
+                            <div className="card mb-3">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Programmer</h5>
+                                    <p className="card-text">Made the webpage!</p>
+                                </div>
                             </div>
                         </div>
+                    </div></div>
+                    <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <h1>Contact us at <span className="badge badge-secondary">FakeEmail!aol.com</span></h1>
                     </div>
                 </div>
-                <div className="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="#"> ArtsAndSuch</a>
-                </div>
-            </footer>
+            </div>
+            <footer class="footer">
+      <div class="container">
+        <span class="text-muted">&copy; 2020, Arts and Such</span>
+      </div>
+    </footer>
         </div>
     );
 }
