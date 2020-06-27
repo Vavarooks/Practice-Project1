1efde0c212113d45d3dafc2703ad64f403fb0cb7
Vavarooks <vavarooks123@gmail.com>
2020-06-27 12:46:47 -0700

    new stuff

--- my-app/src/pages/Programerpage.js
+++ my-app/src/pages/Programerpage.js
@@ -14,21 +14,21 @@ function Coding() {
                     <p>Example works</p>
                     <div className="card-group">
                         <div className="card">
-                            <img src="./images/StockApp.png" className="card-img-top" alt="..." />
+                            <img src="./images/StockApp.jpg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Group Project A Stocks App</h5>
                                 <p className="card-text">https://stormy-refuge-75970.herokuapp.com/</p>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="./images/StarwasApp.png" className="card-img-top" alt="..." />
+                            <img src="./images/StarwasApp.jpg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Group Project Jedi Quiz.</h5>
                                 <p className="card-text">https://fanuelproject2.herokuapp.com/</p>
                             </div>
                         </div>
                         <div className="card">
-                            <img src="./images/WeatherApp.png" className="card-img-top" alt="..." />
+                            <img src="./images/WeatherApp.jpg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">A Basic Weather App</h5>
                                 <p className="card-text">https://vavarooks.github.io/Homework6/</p>
