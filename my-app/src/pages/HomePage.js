18065e885e65082a7368fde9bf240e80161bae60
Vavarooks <vavarooks123@gmail.com>
2020-07-18 10:38:45 -0700

    small edit

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -48,10 +48,6 @@ function Home() {
                 <label for="exampleFormControlInput1">Email address</label>
                 <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
-            <div className="form-group">
-                <label for="exampleFormControlTextarea1">Message</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
