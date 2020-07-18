88dbd0a5bae102e67a8aa4ce758a446cc7dcb7c7
Vavarooks <vavarooks123@gmail.com>
2020-07-18 09:56:03 -0700

    trying to make images pop up from a click

--- my-app/src/pages/Artpage.js
+++ my-app/src/pages/Artpage.js
@@ -8,21 +8,21 @@ function Art() {
             
                 <div className="card-group">
                     <div className="card">
-                        <img src="./images/Example1" className="card-img-top" alt="..." />
+                        <img src="./images/Example1" className="card-img-top" href="./Lineimages.js"/>
                         <div className="card-body">
                             <h5 className="card-title">Line Art</h5>
                             <p className="card-text">Around $4 (Prices may very)</p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/Example2" className="card-img-top" alt="..." />
+                        <img src="./images/Example2" className="card-img-top" href="./ColorImages.js"/>
                         <div className="card-body">
                             <h5 className="card-title">Line and colored</h5>
                             <p className="card-text">Around $10 (Prices may very)</p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/Example3" className="card-img-top" alt="..." />
+                        <img src="./images/Example3" className="card-img-top" href="./Backgroundimages.js"/>
                         <div className="card-body">
                             <h5 className="card-title">Colored art with background</h5>
                             <p className="card-text">Around $15 (Prices may very)</p>
---
+++ my-app/src/pages/ColorImages.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Cimages() {
+    return (
+        <div>
+         <div className="row">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Item Name</h5>
+                            <img src="..." className="card-img-top" alt="..." />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Item Name</h5>
+                            <img src="..." className="card-img-top" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Cimages;
