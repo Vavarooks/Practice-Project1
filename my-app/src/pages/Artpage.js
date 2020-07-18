cc0680b4197a64151d75e68fa5a2b7b94963f5c6
Vavarooks <vavarooks123@gmail.com>
2020-07-18 09:11:01 -0700

    new images

--- my-app/src/pages/Artpage.js
+++ my-app/src/pages/Artpage.js
@@ -8,21 +8,21 @@ function Art() {
             
                 <div className="card-group">
                     <div className="card">
-                        <img src="./images/line-art2.jpg" className="card-img-top" alt="..." />
+                        <img src="./images/Example1" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Line Art</h5>
                             <p className="card-text">Around $4 (Prices may very)</p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/Colored-charater.jpg" className="card-img-top" alt="..." />
+                        <img src="./images/Example2" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Line and colored</h5>
                             <p className="card-text">Around $10 (Prices may very)</p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/color-and-background.jpg" className="card-img-top" alt="..." />
+                        <img src="./images/Example3" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Colored art with background</h5>
                             <p className="card-text">Around $15 (Prices may very)</p>
