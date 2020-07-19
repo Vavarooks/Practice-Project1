b28cabae108b40825b772e7b84a83e057bac23cc
Vavarooks <vavarooks123@gmail.com>
2020-07-19 13:13:18 -0700

    image fixed?

--- my-app/src/pages/Artpage.js
+++ my-app/src/pages/Artpage.js
@@ -5,40 +5,32 @@ function Art() {
     return (
         <div>
             <div className="container">
-
                 <div className="card-group">
                     <div className="card">
-
-                        <a href="./">
+                        <a href="./Limage.html">
                             <img src="./images/Example3.jpg" className="card-img-top" />
                         </a>
-
                         <div className="card-body">
                             <h5 className="card-title">Line Art</h5>
                             <p className="card-text">Around $4 (Prices may very)</p>
-                            <p className="card-text">
-                                <a class="nav-link" href="./Lineimage.js">Images</a>
-                            </p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/Example2.jpg" className="card-img-top" />
+                        <a href="./Cimage.html">
+                            <img src="./images/Example2.jpg" className="card-img-top" />
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">Line and colored</h5>
                             <p className="card-text">Around $10 (Prices may very)</p>
-                            <p className="card-text">
-                                <a class="nav-link" href="./ColorImages.js">Images</a>
-                            </p>
                         </div>
                     </div>
                     <div className="card">
-                        <img src="./images/Example1.png" className="card-img-top" />
+                        <a href="./Bimage.html">
+                            <img src="./images/Example1.jpg" className="card-img-top" />
+                        </a>
                         <div className="card-body">
                             <h5 className="card-title">Colored art with background</h5>
                             <p className="card-text">Around $15 (Prices may very)</p>
-                            <p className="card-text">
-                                <a class="nav-link" href="./Backgroundimages.js">Images</a>
-                            </p>
                         </div>
                     </div>
                 </div>
