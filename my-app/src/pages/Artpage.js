2036a6809d6951efa6dbd3b81e094acf90416a11
Vavarooks <vavarooks123@gmail.com>
2020-07-22 11:41:53 -0700

    Images added

--- my-app/src/pages/Artpage.js
+++ my-app/src/pages/Artpage.js
@@ -18,7 +18,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img  src="./image/Example4.png" className="card-img-top"/>
+                                            <img  src="./images/Example4.png" className="card-img-top"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -27,7 +27,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img src="./image/Example5.jpg" className="card-img-top"/>
+                                            <img src="./images/Example5.jpg" className="card-img-top"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
