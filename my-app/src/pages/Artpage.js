534938c0a60913417c356b08efd048906f69e82c
Vavarooks <vavarooks123@gmail.com>
2020-07-22 15:27:40 -0700

    Images added

--- my-app/src/pages/Artpage.js
+++ my-app/src/pages/Artpage.js
@@ -18,7 +18,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img  src="./images/Example4.png/100px180/?text=Image cap" alt="Card image cap"className="card-img-top"/>
+                                            <img  src="./images/Example4.png" style="width:500px;height:600px;" className="card-img-top"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -27,7 +27,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img src="./images/Example5.jpg/100px180/?text=Image cap" alt="Card image cap"className="card-img-top"/>
+                                            <img src="./images/Example5.jpg" style="width:500px;height:600px;" className="card-img-top"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -55,7 +55,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img className="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap"/>
+                                            <img className="card-img-top" src="..." style="width:500px;height:600px;"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -64,7 +64,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img className="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                                            <img className="card-img-top" src="..." style="width:500px;height:600px;"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -92,7 +92,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img className="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                                            <img className="card-img-top" src="..." style="width:500px;height:600px;"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
@@ -101,7 +101,7 @@ function Art() {
                                     <div className="card">
                                         <div className="card-body">
                                             <h5 className="card-title">Special title treatment</h5>
-                                            <img className="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                                            <img className="card-img-top" src="..." style="width:500px;height:600px;"/>
                                             <p className="card-text">Discription</p>
                                         </div>
                                     </div>
